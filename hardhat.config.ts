05168d02ccad91b2267c6c9582bdc4f2982abc6b
Vladimir Kumalagov <95184524+KumaCrypto@users.noreply.github.com>
2022-08-30 10:39:08 +0200

    End staging test / fix verify

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -25,6 +25,21 @@ const config: HardhatUserConfig = {
 				process.env.POLYGON_API,
 			accounts: [process.env.PRIVATE_KEY as string],
 		},
+
+		goerli: {
+			url:
+				"https://eth-goerli.g.alchemy.com/v2/" +
+				process.env.GOERLI_RPC_URL,
+			accounts: [process.env.GOERLI_PRIVATE_KEY as string],
+			chainId: 5,
+			timeout: 100000,
+		},
+	},
+
+	etherscan: {
+		apiKey: {
+			goerli: process.env.ETHERSCAN_API_KEY as string,
+		},
 	},
 
 	gasReporter: {
@@ -40,7 +55,7 @@ const config: HardhatUserConfig = {
 	},
 
 	mocha: {
-		timeout: 30000,
+		timeout: 10000000,
 	},
 };
 
